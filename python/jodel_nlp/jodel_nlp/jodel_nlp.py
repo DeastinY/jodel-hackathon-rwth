99512afc5fb4083319e200c2507740d2f4ef4a70
DeastinY <polzin.richard@gmail.com>
2017-05-30 12:10:35 +0200

    fixed requirements

--- python/jodel_nlp/jodel_nlp/jodel_nlp.py
+++ python/jodel_nlp/jodel_nlp/jodel_nlp.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3.6
 # -*- coding: utf-8 -*-
+import argparse
+from pathlib import Path
+from nltk.tag.stanford import StanfordNERTagger, StanfordPOSTagger
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+FILE_DB = Path("DB.sqlite")
+
+def extract():
+    pass
+
+
+def tokenize():
+    pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--extract", action="store_true")
+    args = vars(parser.parse_args())
+
