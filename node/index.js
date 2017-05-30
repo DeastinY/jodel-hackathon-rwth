e8ad30c1554acf1af719bccf75dca9101fa49457
biodroid <ceo@biodroid.org>
2017-05-30 13:28:59 +0200

    abstracted fetch

--- node/index.js
+++ node/index.js
@@ -2,16 +2,22 @@ const natural = require('natural'),
   http = require('https'),
   fs = require('fs'),
   URL = require('url'),
-  _config = `./config.json`;
+  _config = `./config.json`,
+  __baseurl = `https://api.go-tellm.com/api/v2`;
 
 const App = class App {
-  constructor(...c) {
+  constructor(config) {
     this.client = null;
-    this.init(...c);
+    this.config = config;
+    this.getPosts().then(console.log)
   }
-  fetch(url, headers, method='GET') {
+  async getPosts() {
+    return this.fetch(`${__baseurl}/posts`).catch(console.error);
+  }
+  fetch(url, headers={}, method='GET') {
     url = URL.parse(url);
     let data = [];
+    headers['Authorization'] = `Bearer ${this.config.accessToken}`;
     return new Promise((resolve,reject)=>{
       http.request({
         protocol: url.protocol,
@@ -23,19 +29,14 @@ const App = class App {
       }, res=>{
         res.on('data', d=>data.push(d));
         res.on('end', _=>{
+          data = data.map(d=>d.toString()).reduce((a,b)=>a+b);
           try {
-            data = JSON.parse(data.map(d=>d.toString()).reduce((a,b)=>a+b));
-            resolve(data);
-          }catch(e){reject(e);}
+            let json = JSON.parse(data);
+            resolve(json);
+          }catch(e){resolve(data);}
         });
       }).end()
     })
-  }
-  async init(config) {
-    let posts = await this.fetch(`https://api.go-tellm.com/api/v2/posts`, {
-      Authorization: `Bearer ${config.accessToken}`
-    }).catch(console.error);
-    console.log(posts)
   }
 }
 const config = fs.existsSync(_config)?require(_config):{};
